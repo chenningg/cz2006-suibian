50d3c82c62174d268d2730ecc13216f9f146b922
Chen Ning <chenningg97@gmail.com>
2020-03-17 15:04:03 +0800

    My app

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const formatName = (user:User):String => user.firstName + ' ' + user.lastName;
+
+type User = {
+  firstName: String,
+  lastName: String
+}
+
+const user:User = {
+  firstName: 'Poop',
+  lastName: 'Perez',
+}
+
+const testEl = (
+  <h1 className="greeting">Hello world!</h1>
+);
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello, {formatName(user)}!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {testEl}
       </header>
     </div>
   );
