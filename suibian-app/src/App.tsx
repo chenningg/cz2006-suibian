2839fbae20b31dc122e29ee3b97faea34fbbead7
Chen Ning <chenningg97@gmail.com>
2020-03-20 17:23:01 +0800

    Changed spacing to 2 tabs

--- suibian-app/src/App.tsx
+++ suibian-app/src/App.tsx
@@ -3,24 +3,24 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-    connectBackEnd = async () => {
-        fetch("/api/test")
-            .then(res => res.json())
-            .then(message => this.setState({ message }));
-    };
+  connectBackEnd = async () => {
+    fetch("/api/test")
+      .then(res => res.json())
+      .then(message => this.setState({ message }));
+  };
 
-    state = { message: "" };
+  state = { message: "" };
 
-    render() {
-        return (
-            <div className="App">
-                <button className="more" onClick={this.connectBackEnd}>
-                    Get More
-                </button>
-                <p>message is {this.state.message}</p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <button className="more" onClick={this.connectBackEnd}>
+          Get More
+        </button>
+        <p>message is {this.state.message}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
