8b57eff0466f71cd0e7834a997f7a0211b0777a0
Anthony <anthonyfong100@gmail.com>
2020-03-22 18:34:13 +0800

    Refactored react routes to explore modularizing app

--- suibian-app/src/App.tsx
+++ suibian-app/src/App.tsx
@@ -1,23 +1,13 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Socket, { socketState } from "./routes/socket";
 
 class App extends Component {
-    connectBackEnd = async () => {
-        fetch("/api/test")
-            .then(res => res.json())
-            .then(message => this.setState({ message }));
-    };
-
-    state = { message: "" };
-
     render() {
         return (
             <div className="App">
-                <button className="more" onClick={this.connectBackEnd}>
-                    Get More
-                </button>
-                <p>message is {this.state.message}</p>
+                <Socket />
             </div>
         );
     }
