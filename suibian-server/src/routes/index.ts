b26ca8775aabfec87f554d180c85a351d9ed80af
Anthony <anthonyfong100@gmail.com>
2020-03-23 15:16:53 +0800

    Fix spacing problem

--- suibian-server/src/routes/index.ts
+++ suibian-server/src/routes/index.ts
@@ -6,7 +6,6 @@ const router = express.Router();
 
 router.use("/api/room", roomRouter);
 router.use("/api/test", testRouter);
-
 router.get("*", (req, res) => {
     res.sendFile(
         path.join(__dirname + "/../../../suibian-app/public/index.html")
