c987a56228aba5ff30868308bec14df98f899850
Anthony <anthonyfong100@gmail.com>
2020-03-23 15:02:34 +0800

    Refactored the socket connections to seperate file

--- suibian-server/src/index.ts
+++ suibian-server/src/index.ts
@@ -2,45 +2,18 @@ import * as dotenv from "dotenv-extended";
 import express from "express";
 import router from "./routes";
 import path from "path";
+import sockets from "./sockets";
 const app = express();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
 
 //defining ports
 const PORT = process.env.PORT || 4000;
 dotenv.load({ errorOnMissing: true, includeProcessEnv: true });
 
-http.listen(PORT, () => {
-    console.log(`Server is listening to port ${PORT}`);
-});
-
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "/../../suibian-app/public")));
 app.use(router);
-app.set("socketio", io);
 
-type socketMessage = {
-    username: string;
-    message: string;
-};
-
-io.on("connection", function(socket: any) {
-    console.log(Object.keys(io.sockets.sockets));
-    socket.on("disconnect", () => console.log("Client disconnected"));
-    socket.on("change_username", (data: socketMessage) => {
-        socket.username = data.message;
-        console.log(`list of all sockets ${Object.keys(io.clients())}`);
-        io.sockets.emit("new_user", {
-            message: socket.username
-        });
-        console.log("user emitted");
-    });
-
-    socket.on("new_message", (data: socketMessage) =>
-        //emits to all clients connected
-        io.sockets.emit("new_message", {
-            message: data.message,
-            username: socket.username
-        })
-    );
+const httpServer = sockets.startSocketServer(app);
+httpServer.listen(PORT, () => {
+    console.log(`Server is listening to port ${PORT}`);
 });
---
+++ suibian-server/src/sockets/index.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import socketio from "socket.io";
+import {
+    joinRoom,
+    joinRoomPayload,
+    roomMessagePayload,
+    broadcastRoom,
+    createRoom
+} from "./room";
+const http = require("http");
+
+export type socketCommands = "joinRoom" | "createRoom" | "changeUsername";
+
+export default {
+    startSocketServer: function(app: express.Router) {
+        const httpServer = http.Server(app);
+        const io = socketio.listen(httpServer);
+
+        io.on("connection", (socket: socketio.Socket) => {
+            console.log(`socket ${socket.id} connected`);
+
+            socket.on("disconnect", () =>
+                console.log(`socket ${socket.id} disconnected`)
+            );
+
+            socket.on("joinRoom", (data: joinRoomPayload) => {
+                joinRoom(socket, io, data);
+            });
+
+            socket.on("broadcastMessage", (data: roomMessagePayload) => {
+                broadcastRoom(io, data);
+            });
+
+            socket.on("createRoom", (data: { username: string }) => {
+                createRoom(socket, data);
+                console.log(io.sockets.adapter.rooms);
+            });
+
+            socket.on("startRoom", (data: roomMessagePayload) => {
+                // startRoom();
+            });
+        });
+
+        return httpServer;
+    }
+};
