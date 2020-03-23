2e662530992d77fe2d3037ce8c5db6fdbfbf17e5
tanchuanxin <57444602+tanchuanxin@users.noreply.github.com>
2020-03-24 03:35:45 +0800

    for loop wip

--- suibian-app/src/components/UserPreferences.tsx
+++ suibian-app/src/components/UserPreferences.tsx
@@ -1,8 +1,16 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "../css/PageBase.css";
 import "../css/UserPreferences.css";
 import { Switch } from "@material-ui/core";
 import NavBar from "./NavBar";
+import { PreferenceItem } from "./PreferenceItem";
+
+const preferences: Array<Preference> = [
+  { type: "Halal" },
+  { type: "Vegetarian" },
+  { type: "Vegan" },
+  { type: "Bhuddhist" }
+];
 
 class UserPreferences extends Component {
   render() {
@@ -13,32 +21,19 @@ class UserPreferences extends Component {
             <NavBar />
           </div>
 
-          <div className="page-container preference-container">
-            <h1>User Preferences</h1>
-            <div className="preference">
-              <p>Halal</p>
-              <p>
-                <Switch></Switch>
-              </p>
-            </div>
-            <div className="preference">
-              <p>Vegetarian</p>
-              <p>
-                <Switch></Switch>
-              </p>
-            </div>
-            <div className="preference">
-              <p>Vegan</p>
-              <p>
-                <Switch></Switch>
-              </p>
-            </div>
-            <div className="preference">
-              <p>Bhuddhist</p>
-              <p>
-                <Switch></Switch>
-              </p>
-            </div>
+          <div className="page-container user-preference-container">
+            {/* <h1>User Preferences</h1> */}
+
+            <PreferenceItem preference={preferences[0]} />
+            <PreferenceItem preference={preferences[1]} />
+            <PreferenceItem preference={preferences[2]} />
+            <PreferenceItem preference={preferences[3]} />
+            <PreferenceItem preference={preferences[0]} />
+            <PreferenceItem preference={preferences[1]} />
+            <PreferenceItem preference={preferences[2]} />
+            <PreferenceItem preference={preferences[3]} />
+            <PreferenceItem preference={preferences[2]} />
+            <PreferenceItem preference={preferences[3]} />
           </div>
         </div>
       </React.Fragment>
