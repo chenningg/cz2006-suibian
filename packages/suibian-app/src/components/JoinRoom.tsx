9922133f0113ff74b88a4adf5cf9103858cec266
Shaun__ <35448957+cpshaun@users.noreply.github.com>
2020-03-25 01:16:26 +0800

    Update JoinRoom.tsx

--- packages/suibian-app/src/components/JoinRoom.tsx
+++ packages/suibian-app/src/components/JoinRoom.tsx
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import "../css/PageBase.css";
 import { Icon } from "@material-ui/core";
 import NavBar from "./NavBar";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import { Link } from "react-router-dom";
 
 class JoinRoom extends Component {
   render() {
@@ -19,20 +16,9 @@ class JoinRoom extends Component {
             <h1>JoinRoom</h1>
           </div>
         </div>
-        <div className="spacer"></div>
-        <div className="spacer"></div>
-        <div className="spacer"></div>
-        <button
-          type="button"
-          className="standard-button red-button fixed-button"
-        >
-          <Link to="/roompagejoin" className="red-button-hyperlink-stripped">
-            Join Room
-          </Link>
-        </button>
-      </div>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
 
 export default JoinRoom;
