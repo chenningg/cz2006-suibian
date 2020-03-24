9de04d7346e3e664458954ea6d5bd94678053db7
Anthony <anthonyfong100@gmail.com>
2020-03-25 01:21:23 +0800

    Removed dist files and removed package json

--- packages/commons/dist/httpStatus.d.ts
+++
@@ -1,7 +0,0 @@
-export declare enum httpStatus {
-    ok = 200,
-    created = 201,
-    badRequest = 400,
-    unauthorized = 401,
-    notFound = 404
-}
--- packages/commons/dist/socketRoutes.d.ts
+++
@@ -1,5 +0,0 @@
-import SocketIOServer from "socket.io";
-export declare type socketCommands = "joinRoom" | "createRoom" | "StartRoom" | "changeUsername" | "broadcastMessage" | "connection" | "disconnect";
-export interface suibianSocketServer extends SocketIOServer.Socket {
-    emit(event: socketCommands, data: any): boolean;
-}
