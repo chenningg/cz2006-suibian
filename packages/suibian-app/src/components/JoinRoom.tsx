1036f42fc3777656452d4512e65e40b9a51794fa
Shaun__ <35448957+cpshaun@users.noreply.github.com>
2020-03-24 21:07:58 +0800

    commit

--- packages/suibian-app/src/components/JoinRoom.tsx
+++ packages/suibian-app/src/components/JoinRoom.tsx
@@ -1,22 +1,60 @@
 import React, { Component } from "react";
+import "../css/JoinRoom.css";
 import "../css/PageBase.css";
 import { Icon } from "@material-ui/core";
 import NavBar from "./NavBar";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 
-class JoinRoom extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div className="main-container">
-          <NavBar />
-
-          <div className="page-container">
-            <h1>JoinRoom</h1>
-          </div>
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "& > *": {
+        margin: theme.spacing(0),
+        width: "41ch"
+      }
+    }
+  })
+);
+function JoinRoom() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <div className="main-container">
+        <NavBar />
+        <div className="spacer"></div>
+        <div className="title">
+          <h1>ROOM CODE:</h1>
         </div>
-      </React.Fragment>
-    );
-  }
+        <div className="text-field">
+          <form className={classes.root} noValidate autoComplete="off">
+            <TextField
+              id="roomcode"
+              label="ENTER 4-LETTER CODE"
+              variant="filled"
+            />
+          </form>
+        </div>
+        <div className="title">
+          <h1>NAME:</h1>
+        </div>
+        <div className="text-field">
+          <form className={classes.root} noValidate autoComplete="off">
+            <TextField id="roomcode" label="ENTER YOUR NAME" variant="filled" />
+          </form>
+        </div>
+        <div className="spacer"></div>
+        <div className="spacer"></div>
+        <div className="spacer"></div>
+        <button
+          type="button"
+          className="standard-button red-button fixed-button"
+        >
+          Join Room
+        </button>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default JoinRoom;
