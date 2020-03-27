ec92472b85c6a33f544d2abfe62c4ceff2fe9779
Shaun__ <35448957+cpshaun@users.noreply.github.com>
2020-03-27 15:23:16 +0800

    Added eATERY

--- packages/suibian-app/src/components/Eatery.tsx
+++ packages/suibian-app/src/components/Eatery.tsx
@@ -1,11 +1,43 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
+import "../css/Eatery.css";
 
 class Eatery extends Component {
+  //State
+  eateryname = "Albert Food Centre";
+  foodstall = "Hock Lee Fishball Noodle";
+
   render() {
     return (
       <>
         <NavBar />
+        <div className="eatery">
+          <div className="app-content flex-container flex-col  flex-center-v">
+            <h2 className="eateryname">{this.eateryname}</h2>
+            <div className="map">
+              <img
+                className="location"
+                style={{ alignContent: "center" }}
+                src="https://picsum.photos/id/1025/350/350"
+              />
+            </div>
+            <a
+              href="https://www.google.com/maps/@${your_lat},${your_lng},${your_desired_zoom}z"
+              className="gmaps-button"
+            >
+              View in Maps
+            </a>
+
+            <text className="foodstall-name">{this.foodstall}</text>
+            <img
+              className="foodstall"
+              src="https://picsum.photos/id/1062/350/200"
+            ></img>
+            <text className="address">
+              #01-94 Albert Centre Market &amp; Food Centre
+            </text>
+          </div>
+        </div>
       </>
     );
   }
