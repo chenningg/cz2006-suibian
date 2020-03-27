45df2dab363fe34d3dffa9dd80e9ec37b8b3f92a
Shaun__ <35448957+cpshaun@users.noreply.github.com>
2020-03-27 17:41:19 +0800

    eatery edits

--- packages/suibian-app/src/components/Eatery.tsx
+++ packages/suibian-app/src/components/Eatery.tsx
@@ -1,9 +1,40 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
 import "../css/Eatery.css";
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
 
-class Eatery extends Component {
+const location = {
+  lat: 47.611036,
+  Long: -93.615641
+};
+const mapStyles = {
+  width: 350,
+  height: 350
+};
+
+class Eatery extends Component<{ google: string }> {
   //State
+  placename = "BIG DICK LAKE";
+  state = {
+    showingInfoWindow: false, //Hides or the shows the infoWindow
+    activeMarker: {}, //Shows the active marker upon click
+    selectedPlace: this.placename //Shows the infoWindow to the selected place upon a marker
+  };
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
   eateryname = "Albert Food Centre";
   foodstall = "Hock Lee Fishball Noodle";
 
@@ -15,11 +46,23 @@ class Eatery extends Component {
           <div className="app-content flex-container flex-col  flex-center-v">
             <h2 className="eateryname">{this.eateryname}</h2>
             <div className="map">
-              <img
-                className="location"
-                style={{ alignContent: "center" }}
-                src="https://picsum.photos/id/1025/350/350"
-              />
+              <Map
+                style={mapStyles}
+                google={this.props.google}
+                zoom={14.7}
+                initialCenter={{ lat: 47.611036, lng: -93.615641 }}
+              >
+                <Marker onClick={this.onMarkerClick} name={"bigDICKlake"} />
+                <InfoWindow
+                  marker={this.state.activeMarker}
+                  visible={this.state.showingInfoWindow}
+                  onClose={this.onClose}
+                >
+                  <div>
+                    <h4>{this.state.selectedPlace}</h4>
+                  </div>
+                </InfoWindow>
+              </Map>
             </div>
             <a
               href="https://www.google.com/maps/@${your_lat},${your_lng},${your_desired_zoom}z"
@@ -42,4 +85,12 @@ class Eatery extends Component {
     );
   }
 }
-export default Eatery;
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyDEaux00JCnvfiaqExfJHY5cu-oe8fSOxA"
+})(Eatery);
+// <img
+//   className="location"
+//   style={{ alignContent: "center" }}
+//   src="https://picsum.photos/id/1025/350/350"
+// />;
