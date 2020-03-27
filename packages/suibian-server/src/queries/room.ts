1b48ed4ddbf9257247ab2639ed2c82ad5e056cf2
Anthony <anthonyfong100@gmail.com>
2020-03-27 20:30:56 +0800

    Remove all deprecated codes

--- packages/suibian-server/src/queries/room.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import path from "path";
-const router = express.Router();
-
-router.post("/:roomnumber", (req, res) => {
-    console.log(req.params);
-});
-
-export default router;
--- packages/suibian-server/src/routes/index.ts
+++ packages/suibian-server/src/routes/index.ts
@@ -1,11 +1,7 @@
 import express from "express";
 import path from "path";
-import roomRouter from "../queries/room";
-import testRouter from "../queries/testRouter";
 const router = express.Router();
 
-router.use("/api/room", roomRouter);
-router.use("/api/test", testRouter);
 router.get("*", (req, res) => {
     res.sendFile(
         path.join(__dirname + "/../../../suibian-app/public/index.html")
