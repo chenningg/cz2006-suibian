3fefa2c05cc3c4dec038a90a69af537915f2ff44
Chen Ning <chenningg97@gmail.com>
2020-03-29 02:17:29 +0800

    Redux Sockets

--- packages/suibian-app/src/components/CreateRoom.tsx
+++ packages/suibian-app/src/components/CreateRoom.tsx
@@ -1,5 +1,5 @@
 //app components
-import React, { Component } from "react";
+import React, { Component, ChangeEvent, FormEvent } from "react";
 import NavBar from "./NavBar";
 
 //other components
@@ -8,31 +8,38 @@ import { Link } from "react-router-dom";
 //css
 import "../css/CreateRoom.css";
 
-class CreateRoom extends Component {
-  // onChange = (e: ChangeEvent<HTMLInputElement>) => {
-  //   // @ts-ignore */
-  //   this.setState({ [e.target.id]: e.target.value });
-  // };
+// Sockets and Redux
+import * as SocketTypes from "../types/SocketState";
+import { connect } from "react-redux";
+import ReduxState from "../types/ReduxState";
 
-  // changeUsername = () => {
-  //   if (this.state.socket) {
-  //     this.state.socket.emit("changeUsername", {
-  //       userName: this.state.username,
-  //       message: this.state.username
-  //     });
-  //   }
-  // };
+// Types
+type StateProps = {
+  socketState: SocketTypes.SocketState;
+};
 
-  // createRoom = (e: FormEvent) => {
-  //   e.preventDefault();
-  //   if (this.state.socket) {
-  //     this.changeUsername();
-  //     this.state.socket.emit("createRoom", {
-  //       username: this.state.username,
-  //       roomCode: this.state.roomCode
-  //     });
-  //   }
-  // };
+type DispatchProps = {
+  updateSocketState: (
+    key: string,
+    value: string | number | SocketTypes.SuibianSocket
+  ) => void;
+};
+
+type Props = StateProps & DispatchProps;
+
+class CreateRoom extends Component<Props> {
+  handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    this.props.updateSocketState("username", e.target.value);
+  };
+
+  createRoom = (e: FormEvent) => {
+    e.preventDefault();
+    if (this.props.socketState.socket) {
+      this.props.socketState.socket.emit("createRoom", {
+        username: this.props.socketState.username
+      });
+    }
+  };
 
   render() {
     return (
@@ -41,12 +48,9 @@ class CreateRoom extends Component {
         <div className="create-room">
           <div className="app-content flex-container flex-col flex-center-h flex-center-v">
             <h1 className="title">Create room</h1>
-            <form
-              className="create-room-form"
-              // onSubmit={e => this.createRoom(e)}
-            >
+            <form className="create-room-form" onSubmit={this.createRoom}>
               <input
-                // onChange={e => this.onChange(e)}
+                onChange={this.handleChange}
                 id="username"
                 placeholder="Username"
                 className="username-input"
@@ -68,4 +72,24 @@ class CreateRoom extends Component {
   }
 }
 
-export default CreateRoom;
+// Redux functions
+const mapStateToProps = (state: ReduxState): StateProps => {
+  return {
+    socketState: state.socketState
+  };
+};
+
+// Links a dispatch function to a prop
+const mapDispatchToProps = (dispatch: any): DispatchProps => {
+  return {
+    updateSocketState: (key, value) => {
+      dispatch({
+        type: "UPDATE_SOCKET_STATE",
+        key: key,
+        value: value
+      });
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateRoom);
--- packages/suibian-server/src/sockets/index.ts
+++ packages/suibian-server/src/sockets/index.ts
@@ -3,56 +3,56 @@ import socketio from "socket.io";
 import { joinRoom, createRoom, closeRoom, startRoom } from "./helper/room";
 import { broadcastRoom } from "./helper/messaging";
 import {
-    suibianSocket,
-    joinRoomPayload,
-    roomMessagePayload
+  suibianSocket,
+  joinRoomPayload,
+  roomMessagePayload
 } from "@suibian/commons";
 const http = require("http");
 
 export default {
-    startSocketServer: function(app: express.Router) {
-        const httpServer = http.Server(app);
-        const io = socketio.listen(httpServer);
+  startSocketServer: function(app: express.Router) {
+    const httpServer = http.Server(app);
+    const io = socketio.listen(httpServer);
 
-        io.on("connection", (socket: suibianSocket) => {
-            console.log(`socket ${socket.id} connected`);
+    io.on("connection", (socket: suibianSocket) => {
+      console.log(`socket ${socket.id} connected`);
 
-            socket.on("disconnect", () =>
-                console.log(`socket ${socket.id} disconnected`)
-            );
+      socket.on("disconnect", () =>
+        console.log(`socket ${socket.id} disconnected`)
+      );
 
-            socket.on("joinRoom", (data: joinRoomPayload) => {
-                joinRoom(socket, io, data);
-            });
+      socket.on("joinRoom", (data: joinRoomPayload) => {
+        joinRoom(socket, io, data);
+      });
 
-            socket.on("closeRoom", (data: { roomcode: string }) => {
-                const { roomcode } = data;
-                closeRoom(io, socket, roomcode);
-            });
+      socket.on("closeRoom", (data: { roomcode: string }) => {
+        const { roomcode } = data;
+        closeRoom(io, socket, roomcode);
+      });
 
-            socket.on("broadcastMessage", (data: roomMessagePayload) => {
-                broadcastRoom(io, data);
-            });
+      socket.on("broadcastMessage", (data: roomMessagePayload) => {
+        broadcastRoom(io, data);
+      });
 
-            socket.on("createRoom", async (data: { username: string }) => {
-                //first user creates a room and also joins the room
-                const { username } = data;
-                const roomcode = await createRoom(socket);
-                if (roomcode) {
-                    joinRoom(socket, io, { username, roomcode });
-                }
-            });
+      socket.on("createRoom", async (data: { username: string }) => {
+        //first user creates a room and also joins the room
+        const { username } = data;
+        const roomcode = await createRoom(socket);
+        if (roomcode) {
+          joinRoom(socket, io, { username, roomcode });
+        }
+      });
 
-            socket.on("startRoom", (data: { roomcode: string }) => {
-                const { roomcode } = data;
-                startRoom(io, roomcode);
-            });
+      socket.on("startRoom", (data: { roomcode: string }) => {
+        const { roomcode } = data;
+        startRoom(io, roomcode);
+      });
 
-            socket.on("getRoomInfo", (data: { roomcode: string }) => {
-                getRoomInfo(io, data);
-            });
-        });
+      socket.on("getRoomInfo", (data: { roomcode: string }) => {
+        getRoomInfo(io, data);
+      });
+    });
 
-        return httpServer;
-    }
+    return httpServer;
+  }
 };
