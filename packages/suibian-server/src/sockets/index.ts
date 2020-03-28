fd727389231c151de4bb4bd2d0db29048fd96722
reubenwong97 <reuben.wong.ys@gmail.com>
2020-03-28 21:56:51 +0800

    onemap

--- packages/suibian-server/src/sockets/index.ts
+++ packages/suibian-server/src/sockets/index.ts
@@ -1,57 +1,56 @@
 import express from "express";
 import socketio from "socket.io";
 import {
-    joinRoom,
-    broadcastRoom,
-    createRoom,
-    getRoomInfo,
-    closeRoom
+  joinRoom,
+  broadcastRoom,
+  createRoom,
+  getRoomInfo,
+  closeRoom
 } from "./room";
 import {
-    suibianSocket,
-    joinRoomPayload,
-    roomMessagePayload
+  suibianSocket,
+  joinRoomPayload,
+  roomMessagePayload
 } from "@suibian/commons";
 const http = require("http");
 
 export default {
-    startSocketServer: function(app: express.Router) {
-        const httpServer = http.Server(app);
-        const io = socketio.listen(httpServer);
+  startSocketServer: function(app: express.Router) {
+    const httpServer = http.Server(app);
+    const io = socketio.listen(httpServer);
 
-        io.on("connection", (socket: suibianSocket) => {
-            console.log(`socket ${socket.id} connected`);
+    io.on("connection", (socket: suibianSocket) => {
+      console.log(`socket ${socket.id} connected`);
+      socket.on("disconnect", () =>
+        console.log(`socket ${socket.id} disconnected`)
+      );
 
-            socket.on("disconnect", () =>
-                console.log(`socket ${socket.id} disconnected`)
-            );
+      socket.on("joinRoom", (data: joinRoomPayload) => {
+        joinRoom(socket, io, data);
+      });
 
-            socket.on("joinRoom", (data: joinRoomPayload) => {
-                joinRoom(socket, io, data);
-            });
+      socket.on("closeRoom", (data: { roomcode: string }) => {
+        const { roomcode } = data;
+        closeRoom(io, socket, roomcode);
+      });
 
-            socket.on("closeRoom", (data: { roomcode: string }) => {
-                const { roomcode } = data;
-                closeRoom(io, socket, roomcode);
-            });
+      socket.on("broadcastMessage", (data: roomMessagePayload) => {
+        broadcastRoom(io, data);
+      });
 
-            socket.on("broadcastMessage", (data: roomMessagePayload) => {
-                broadcastRoom(io, data);
-            });
+      socket.on("createRoom", (data: { username: string }) => {
+        createRoom(socket);
+      });
 
-            socket.on("createRoom", (data: { username: string }) => {
-                createRoom(socket);
-            });
+      socket.on("startRoom", (data: roomMessagePayload) => {
+        // startRoom();
+      });
 
-            socket.on("startRoom", (data: roomMessagePayload) => {
-                // startRoom();
-            });
+      socket.on("getRoomInfo", (data: { roomcode: string }) => {
+        getRoomInfo(io, data);
+      });
+    });
 
-            socket.on("getRoomInfo", (data: { roomcode: string }) => {
-                getRoomInfo(io, data);
-            });
-        });
-
-        return httpServer;
-    }
+    return httpServer;
+  }
 };
