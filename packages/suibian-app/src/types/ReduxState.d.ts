f96374a6c53c6ea8f3b35eb6fb10dcdcf470f6c9
Chen Ning <chenningg97@gmail.com>
2020-03-30 01:35:52 +0800

    Updated Maps

--- packages/suibian-app/src/types/ReduxState.d.ts
+++ packages/suibian-app/src/types/ReduxState.d.ts
@@ -1,9 +1,11 @@
 import { SocketState } from "./SocketState";
 // import { User, Vote } from "@suibian/commons";
+import { Position } from "@suibian/commons";
 
 type ReduxState = {
   users: User[]; // Users in a room
   user: User; // Current user data
+  position: Position;
   socketState: SocketState;
   votes: Votes;
   foods: Food[];
