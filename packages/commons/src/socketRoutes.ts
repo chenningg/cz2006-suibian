7980ef33d490c79ee2d137172c070aa2099f9d4a
Anthony <anthonyfong100@gmail.com>
2020-03-29 14:45:31 +0800

    Refactored socket exports to include in types

--- packages/commons/src/socketRoutes.ts
+++ packages/commons/src/socketRoutes.ts
@@ -1,30 +1,38 @@
 import SocketIOServer from "socket.io";
 
 export type socketCommands =
-  | "joinRoom"
-  | "createRoom"
-  | "StartRoom"
-  | "changeUsername"
-  | "broadcastMessage"
-  | "connection"
-  | "disconnect"
-  | "socketError";
+    | "joinRoom"
+    | "createRoom"
+    | "StartRoom"
+    | "changeUsername"
+    | "broadcastMessage"
+    | "connection"
+    | "disconnect"
+    | "socketError";
+
+export type roomPayloadBase = {
+    roomCode: string;
+};
+
+export type createRoomPayload = {
+    username: string;
+};
 
 export type joinRoomPayload = {
-  username: string;
-  roomCode: string;
+    roomCode: string;
+    username: string;
 };
 
 export type roomMessagePayload = {
-  username: string;
-  message: string;
-  roomCode: string;
+    username: string;
+    message: string;
+    roomCode: string;
 };
 
 export interface suibianSocket extends SocketIOServer.Socket {
-  emit(
-    event: socketCommands,
-    data: any,
-    callback?: (params?: any) => void
-  ): boolean;
+    emit(
+        event: socketCommands,
+        data: any,
+        callback?: (params?: any) => void
+    ): boolean;
 }
--- packages/suibian-server/src/sockets/index.ts
+++ packages/suibian-server/src/sockets/index.ts
@@ -3,50 +3,51 @@ import socketio from "socket.io";
 import { joinRoom, createRoom, closeRoom, startRoom } from "./helper/room";
 import { broadcastRoom } from "./helper/messaging";
 import {
-  suibianSocket,
-  joinRoomPayload,
-  roomMessagePayload
+    suibianSocket,
+    joinRoomPayload,
+    roomPayloadBase,
+    createRoomPayload
 } from "@suibian/commons";
 const http = require("http");
 
 export default {
-  startSocketServer: function(app: express.Router) {
-    const httpServer = http.Server(app);
-    const io = socketio.listen(httpServer);
+    startSocketServer: function(app: express.Router) {
+        const httpServer = http.Server(app);
+        const io = socketio.listen(httpServer);
 
-    io.on("connection", (socket: suibianSocket) => {
-      console.log(`socket ${socket.id} connected`);
+        io.on("connection", (socket: suibianSocket) => {
+            console.log(`socket ${socket.id} connected`);
 
-      socket.on("disconnect", () =>
-        console.log(`socket ${socket.id} disconnected`)
-      );
+            socket.on("disconnect", () =>
+                console.log(`socket ${socket.id} disconnected`)
+            );
 
-      socket.on("joinRoom", async (data: joinRoomPayload) => {
-        await joinRoom(socket, io, data);
-      });
+            socket.on("joinRoom", async (data: joinRoomPayload) => {
+                await joinRoom(socket, io, data);
+            });
 
-      socket.on("closeRoom", (data: { roomCode: string }) => {
-        const { roomCode } = data;
-        closeRoom(io, socket, roomCode);
-      });
+            socket.on("closeRoom", (data: roomPayloadBase) => {
+                const { roomCode } = data;
+                closeRoom(io, socket, roomCode);
+            });
 
-      socket.on("createRoom", async (data: { username: string }) => {
-        //first user creates a room and also joins the room
-        const { username } = data;
-        const roomCode = await createRoom(socket);
-        if (roomCode) {
-          await joinRoom(socket, io, { username, roomCode });
-        }
-      });
+            socket.on("createRoom", async (data: createRoomPayload) => {
+                //first user creates a room and also joins the room
+                const { username } = data;
+                const roomCode = await createRoom(socket);
+                if (roomCode) {
+                    await joinRoom(socket, io, { username, roomCode });
+                }
+            });
 
-      socket.on("startRoom", (data: { roomCode: string }) => {
-        const { roomCode } = data;
-        startRoom(io, roomCode);
-      });
+            socket.on("startRoom", (data: roomPayloadBase) => {
+                const { roomCode } = data;
+                startRoom(io, roomCode);
+            });
 
-      socket.on("getRoomInfo", (data: { roomCode: string }) => {});
-    });
+            socket.on("getRoomInfo", (data: roomPayloadBase) => {});
+        });
 
-    return httpServer;
-  }
+        return httpServer;
+    }
 };
