4474e4bc899953da99fd760c810fa9369d167a79
Reuben Wong <reuben.wong.ys@gmail.com>
2020-03-29 16:35:17 +0800

    doing voting

--- packages/suibian-server/src/queries/vote.ts
+++ packages/suibian-server/src/queries/vote.ts
@@ -1,20 +1,27 @@
 import Vote from "../models/vote.model";
 import { isColString } from "sequelize/types/lib/utils";
 
+//TODO: when front end sends votes over, then set user to finished voting
+
 export const createVoteQuery = async (
   username: string,
-  roomcode: string
+  roomcode: string,
+  foodName: string,
+  vote: boolean
 ): Promise<void> => {
   try {
     await Vote.create({
       username,
       roomcode,
-      finishedvote: false
+      foodName,
+      vote // true or false representing whether they like the food
     });
   } catch (err) {
     console.log(err);
   }
 };
+
+export const createVoteBulkQuery = async();
 
 export const checkVoteQuery = async (
   username: string,
